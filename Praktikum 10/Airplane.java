d3bc11f01ee11b3a958f067835382b6273ce9af0
Demas5z <160566175+Demas5z@users.noreply.github.com>
2024-06-04 10:30:24 +0700

    Add files via upload

---
+++ Praktikum 10/Airplane.java
@@ -0,0 +1,15 @@
+public abstract class Airplane extends Vehicle {
+    private double maxLoad;
+
+    public Airplane(double maxLoad) {
+        this.maxLoad = maxLoad;
+    }
+
+    public abstract double calcFuelEfficiency();
+    
+    
+    public abstract double calcTripDistance();
+
+  
+    public abstract String toString();
+}
