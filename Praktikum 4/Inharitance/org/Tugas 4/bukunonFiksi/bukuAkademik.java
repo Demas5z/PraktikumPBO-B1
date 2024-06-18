29d827a4f47b6fd004aec60987be7eef9d412933
Demas5z <160566175+Demas5z@users.noreply.github.com>
2024-06-18 16:29:07 +0700

    Add files via upload

---
+++ Praktikum 4/Inharitance/org/Tugas 4/bukunonFiksi/bukuAkademik.java
@@ -0,0 +1,14 @@
+/*
+    Nama        : Demas Haikal Azizi
+    NIM         : 24060122140161
+    Nama File   : bukuAkademik.java
+    Deskripsi   : File Class dari bukuAkademik.java
+*/
+
+package bk.bukunonFiksi;
+
+public class bukuAkademik extends bukuNonFiksi{
+    public bukuAkademik(String judul, String penulis, String tahunPenerbit, String subjek, int jumlahHalaman, double harga){
+        super(judul, penulis, tahunPenerbit, subjek, jumlahHalaman, harga);
+    }
+}
